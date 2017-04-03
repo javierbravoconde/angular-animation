457f8854bf0834b2909473ad198819c0be0290e0
Javier Bravo Conde <Javier Bravo Conde>
2017-04-03 00:28:46 -0600

    first visibility test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, animate, transition, style } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [],
+  styles: [
+    `.alert { margin-top: 10px; }`
+  ],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            opacity: 1
+        })),
+        state('hidden', style({
+            opacity: 0
+        })),
+        transition('* => *', animate('.5s'))
+    ])
+  ],
+  template: `
+  <div class="container">
+    <h1>Animations</h1>
+    <hr>
+    <button type="button"
+        class="btn btn-primary btn-lg"
+        (click)="toggleVisibility()">
+      {{ getLabel() }}
+    </button>
+      {{ getLabel() }}    
+    <div [@visibility]="visibilitystate" class="alert alert-success">
+      Animate good times! Come on!
+    </div>
+  </div>
+  `
 })
+
+
+
 export class AppComponent {
-  title = 'app works!';
+  visibilitystate = 'shown';
+
+  getLabel() {
+    return this.visibilitystate == 'shown' ? 'Hide' : 'Show';
+  }
+
+  toggleVisibility() {
+    this.visibilitystate = this.visibilitystate == 'shown' ? 'hidden' : 'shown';
+  }
 }
